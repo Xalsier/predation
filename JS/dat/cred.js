5582baa8ae98aa40aac71109ff3a0b0ae86b4d73
Xalsier <81521840+Xalsier@users.noreply.github.com>
2024-03-22 00:16:06 -0400

    Made changes to credits css resolves #6

--- JS/dat/cred.js
+++ JS/dat/cred.js
@@ -1,20 +1,13 @@
 const creditsData = {
     "Site Artwork": [
-        { title: "Background", imageUrl: "path/to/profile-image.jpg", link: "", name: "Dimi" },
-        { title: "Logo", imageUrl: "path/to/logo-image.jpg", link: "", name: "Yuuika Hirasaki" }
+        { title: "Background Art:", imageUrl: "https://pbs.twimg.com/profile_images/1700691574412201984/OUT3LAlp_400x400.jpg", link: "https://twitter.com/dimidostoevsky", name: "Dimi" },
+        { title: "Samsara Logo:", imageUrl: "https://fiverr-res.cloudinary.com/image/upload/f_auto,q_auto,t_profile_original/v1/attachments/profile/photo/5a62feb18531ff03a6fa8c502ad10f76-1686763029082/710fb114-4d28-4f88-9538-779dbe15ed7d.png", link: "https://www.fiverr.com/nekkonimestore", name: "Yuuika Hirasaki" }
     ],
     "Chapter Art": [
-        { title: "Chapter 1", imageUrl: "path/to/maxorcito-image.jpg", link: "", name: "Maxorcito" },
-        { title: "Chapter 2", imageUrl: "path/to/xalsier-image.jpg", link: "", name: "Xalsier" }
+        { generalist: true, imageUrl: ["https://fiverr-res.cloudinary.com/image/upload/f_auto,q_auto,t_profile_original/v1/attachments/profile/photo/fb93b3e0f174081a85806cef70f81a3b-1691187786611/6d8be301-6cd4-4905-b123-f40161b7563e.png", "../assets/xal.jpeg"], link: ["https://www.fiverr.com/maxtorcito", "https://twitter.com/Zarushia"], name: ["Maxorcito", "Xalsier"] }
     ],
     "Character Concept Art": [
-        { title: "Killian", imageUrl: "path/to/maxorcito-image.jpg", link: "", name: "ShadowxxFox" },
-        { title: "Killian", imageUrl: "path/to/Fossil.jpg", link: "", name: "Fossil" },
-        { title: "Naori", imageUrl: "path/to/ChosenComics.jpg", link: "", name: "ChosenComics" },
-        { title: "Sera", imageUrl: "path/to/NicoStarlight.jpg", link: "https://twitter.com/Xalsier", name: "NicoStarlight" }
-    ],
-    "Special Thanks": [
-        { title: "To", imageUrl: "path/to/cosmic-image.jpg", link: "https://open.spotify.com/artist/7rnbJU1HeUoxnYVdBXM4Wb?si=c6Ee2MjcSSGBTrBpyRTCZw", name: "Cosmic" }
+        { generalist: true, imageUrl: ["https://storage.ko-fi.com/cdn/useruploads/ec3bca40-97c8-4f05-ba72-140bd7814fca.jpeg", "https://fiverr-res.cloudinary.com/image/upload/f_auto,q_auto,t_profile_original/v1/attachments/profile/photo/8e200b84fec56b4b6a9653c014039ac3-1636933825862/d5a7fdf9-36b7-44ad-ab5e-197ddc9851b3.jpg"], link: ["https://ko-fi.com/shadowxxfox", "https://www.fiverr.com/unagiroll"], name: ["ShadowxxFox", "NicoStarlight"] }
     ]
 };
 
@@ -24,30 +17,56 @@ function generateCredits(creditsData) {
         const column = document.createElement('div');
         column.classList.add('credits_column');
 
-        const heading = document.createElement('strong');
+        const heading = document.createElement('h4');
         heading.textContent = section;
         column.appendChild(heading);
 
-        creditsData[section].forEach(item => {
+        const sectionData = creditsData[section];
+        if (sectionData.length > 0 && sectionData[0].generalist) {
             const profile = document.createElement('div');
             profile.classList.add('profile');
 
-            const title = document.createElement('p');
-            title.textContent = item.title;
-            profile.appendChild(title);
+            sectionData[0].imageUrl.forEach((url, index) => {
+                if (url != null) {
+                    const img = document.createElement('img');
+                    img.src = url;
+                    img.alt = `${sectionData[0].name[index]} Profile`;
+                    profile.appendChild(img);
 
-            const img = document.createElement('img');
-            img.src = item.imageUrl;
-            img.alt = `${item.title} Profile`;
-            profile.appendChild(img);
-
-            const link = document.createElement('a');
-            link.href = item.link;
-            link.textContent = item.name; // Use the 'name' field for the link text
-            profile.appendChild(link);
+                    const link = document.createElement('a');
+                    link.href = sectionData[0].link[index];
+                    link.textContent = sectionData[0].name[index];
+                    profile.appendChild(link);
+                }
+            });
 
             column.appendChild(profile);
-        });
+        } else {
+            sectionData.forEach(item => {
+                const profile = document.createElement('div');
+                profile.classList.add('profile');
+
+                const title = document.createElement('strong');
+                title.textContent = item.title;
+                profile.appendChild(title);
+
+                if (item.imageUrl != null) {
+                    const img = document.createElement('img');
+                    img.src = item.imageUrl;
+                    img.alt = `${item.title} Profile`;
+                    profile.appendChild(img);
+                }
+
+                if (item.link != null) {
+                    const link = document.createElement('a');
+                    link.href = item.link;
+                    link.textContent = item.name;
+                    profile.appendChild(link);
+                }
+
+                column.appendChild(profile);
+            });
+        }
 
         container.appendChild(column);
     }
