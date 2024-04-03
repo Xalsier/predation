b78ff2b3eab7b0d06c64a0283673b35e3fcbe9d7
Xalsier <81521840+Xalsier@users.noreply.github.com>
2024-04-03 16:50:03 -0400

    Rolling out very small introduction of profiles with dialog.

--- JS/util/parse.js
+++ JS/util/parse.js
@@ -1,7 +1,15 @@
 export function simpleMarkdownParse(text) {
     let html = text;
+
+    // Basic formatting (existing code)
     html = html.replace(/\n/g, '<br/>');
     html = html.replace(/\*\*([^\*]+)\*\*/g, '<strong>$1</strong>');
     html = html.replace(/\*([^\*]+)\*/g, '<em>$1</em>');
+
+    // Profile image formatting
+    html = html.replace(/FUWA:/g, '<span class="webnovel-profile-img"><img src="https://hunt.predation.jp/CA/1.png" alt="Fuwa Profile"></span> FUWA:'); 
+    html = html.replace(/NAORI:/g, '<span class="webnovel-profile-img"><img src="https://hunt.predation.jp/CA/2.png" alt="Fuwa Profile"></span> NAORI:'); 
+
     return html;
 }
+
